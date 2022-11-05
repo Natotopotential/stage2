95aaf0664823c59bc335faa534a60f4a3d821a7d
Natotopotential <nathan752l@gmail.com>
2022-11-05 08:12:05 +0100

    initial commit

--- src/Link.js
+++ src/Link.js
@@ -12,55 +12,55 @@ function LinkList() {
       >
         Twitter Link
       </Link> */}
-      <Link
-        to='https://twitter.com/nath7521'
+      <a
+        href='https://twitter.com/nath7521'
         target='_blank'
         id='btn__twitter'
         className='btn'
       >
         Twitter Link
-      </Link>
-     
-      <Link
-        to='https://training.zuri.team/'
+      </a>
+
+      <a
+        href='https://training.zuri.team/'
         target='_blank'
         id='btn__zuri'
         className='btn'
       >
         Zuri Team
-      </Link>
-      <Link
-        to='https://books.zuri.team/'
+      </a>
+      <a
+        href='https://books.zuri.team/'
         target='_blank'
         id='books'
         className='btn'
       >
         Zuri Books
-      </Link>
-      <Link
-        to='https://books.zuri.team/python-for-beginners?ref_id=nath'
+      </a>
+      <a
+        href='https://books.zuri.team/python-for-beginners?ref_id=nath'
         target='_blank'
         id='book__python'
         className='btn'
       >
         Python Book
-      </Link>
-      <Link
-        to='https://background.zuri.team/'
+      </a>
+      <a
+        href='https://background.zuri.team/'
         target='_blank'
         id='pitch'
         className='btn'
       >
         Checks for Coders
-      </Link>
-      <Link
-        to='https://books.zuri.team/design-rules'
+      </a>
+      <a
+        href='https://books.zuri.team/design-rules'
         target='_blank'
         id='book__design'
         className='btn'
       >
         Books
-      </Link>
+      </a>
       <Link to='/contact' target='_blank' id='contact' className='btn'>
         Contact Me
       </Link>
